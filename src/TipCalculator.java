94baf185ce8555bc3af0c69016b1401fa6f4f9be
BT_4N2_33 <zwu2542@bths.edu>
2022-09-21 11:43:46 -0400

    Finished initial variables for tip percent, no. of people, and initial bill amount.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,5 +4,9 @@ public class TipCalculator {
         Scanner s = new Scanner(System.in);
         System.out.println("How many people are with you?");
         int people = s.nextInt();
+        System.out.println("What tip percent would you like to input?");
+        int tip = s.nextInt();
+        System.out.println("What is your total bill right now?");
+        double bill = s.nextDouble();
     }
 }
