f7bd15899e2d00f34c5c2e00eb7bae3e3f842eef
BT_4N2_33 <zwu2542@bths.edu>
2022-09-21 11:38:59 -0400

    New calculator project. Adding code for first few variables for later use.

---
+++ src/TipCalculator.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("How many people are with you?");
+        int people = s.nextInt();
+    }
+}
